f7f2d264de1f4a2713c0cdb3fa69b682cf1ebedf
lestherruiz <lruiz0312@hotmail.com>
2023-05-06 23:02:03 -0400

    Card Example Added

--- src/App.jsx
+++ src/App.jsx
@@ -5,22 +5,30 @@ import './App.css'
 import NavigationBar from './components/NavigationBar'
 
 import 'bootstrap/dist/css/bootstrap.min.css';
-import { Container } from 'react-bootstrap'
+import { Container, Stack } from 'react-bootstrap'
 import Temblores from './components/Temblores'
 import UserForm from './components/UserForm'
 import ParentComponent from './components/ParentChildExample/ParentComponent'
+import CardExample from './components/CardExample/CardExample'
 
 function App() {
   const [count, setCount] = useState(0)
 
   return (
     <>
-    <NavigationBar />
-    <Container>
-      {/* <Temblores /> */}
-      <UserForm />
-      {/* <ParentComponent /> */}
-    </Container>
+      <NavigationBar />
+      <Container style={{ padding: 20 }}>
+        {/* <Temblores /> */}
+        {/* <UserForm /> */}
+        {/* <ParentComponent /> */}
+        <Stack direction='horizontal' gap={2}>
+          <CardExample ShowButton='false' ShowDescription='true' Description="Some quick example text to build on the card title and make up the
+            bulk of the card's content." ShowTitle='true' Title='Card Example!' />
+          <CardExample ShowButton='true' ShowDescription='false' Description="Some quick example text to build on the card title and make up the
+            bulk of the card's content." ShowTitle='true' Title='Card Example!' />
+        </Stack>
+      </Container>
+
     </>
   )
 }
